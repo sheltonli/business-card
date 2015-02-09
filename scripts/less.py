b69553f1d1dba4c26d5b45dbb4aff5d2183a1630
sheltonli <shelton.li.ts@gmail.com>
2015-02-09 15:40:07 -0500

    Added less, minor updates elsewhere

---
+++ scripts/less.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import datetime
+import os
+import re
+import subprocess
+import sys
+import time
+from watchdog.events import FileSystemEventHandler, FileSystemEvent
+from watchdog.observers import Observer
+
+#-------------------------------------------------------------------------------
+# less.py configuration
+#-------------------------------------------------------------------------------
+
+PATTERN = re.compile('.*\.less$')
+
+PATH = 'static/css'
+
+FILES = [
+    ('app.less', 'app.css'),
+]
+
+#-------------------------------------------------------------------------------
+
+
+class EventHandler(FileSystemEventHandler):
+
+    def dispatch(self, event):
+        if event.is_directory or not PATTERN.match(event.src_path):
+            return
+
+        for input_path, output_path in FILES:
+            input_path = os.path.join(PATH, input_path)
+            output = open(os.path.join(PATH, output_path), 'wb')
+
+            subprocess.call(['lessc', '-x', input_path], stdout=output)
+
+            now = datetime.datetime.now().strftime('%H:%M:%S')
+            print('{} - compiled {}'.format(now, output_path))
+
+
+if __name__ == '__main__':
+    EventHandler().dispatch(FileSystemEvent('initial compile', 'static/css/app.less'))
+
+    if len(sys.argv) > 1 and sys.argv[1] == '--no-watch':
+        sys.exit(0)
+
+    observer = Observer()
+    observer.schedule(EventHandler(), path=PATH, recursive=True)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        observer.stop()
+
+    observer.join()
